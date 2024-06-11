320e96abd67e36dfeee9d3cafb73814d930df9db
beks2016 <lbaik.abdellah@gmail.com>
2024-06-11 23:15:32 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const { Storage } = require('@google-cloud/storage');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const storage = new Storage();
-const bucketName = process.env.BUCKET_NAME;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
 });
 
-app.post('/upload', async (req, res) => {
-  const fileContent = req.body.file_content;
-  const bucket = storage.bucket(bucketName);
-  const file = bucket.file('uploaded_file.txt');
-
-  await file.save(fileContent);
-  res.send('File uploaded!');
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
 });
